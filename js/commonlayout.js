a8dec250ba66956361d0db1912ada219ca68f004
ddiong2525 <choijw2005@naver.com>
2025-05-19 20:30:04 +0900

    0519-8

--- js/commonlayout.js
+++ js/commonlayout.js
@@ -22,4 +22,17 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         );
     });
+
+    $('.mp_list li').on('click', function () {
+        // li에서 on 제거 후, 클릭된 li에 on 추가
+        $('.mp_list li').removeClass('on');
+        $(this).addClass('on');
+
+        // 클래스명 추출 (on 제외)
+        const className = $(this).attr('class').split(' ').find(cls => cls !== 'on');
+
+        // mp_right 안에 있는 div의 on 제거 후, 해당 클래스에 on 추가
+        $('.mp_right > div').removeClass('on');
+        $('.mp_right > .' + className).addClass('on');
+    });
 });
