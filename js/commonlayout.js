f1a4f41bb14d78523cb90c6f02fe85117003ebdd
ddiong2525 <choijw2005@naver.com>
2025-05-19 14:44:24 +0900

    0519-5

--- js/commonlayout.js
+++ js/commonlayout.js
@@ -9,4 +9,17 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     });
+
+    $(function () {
+        $('.list_right ul li').hover(
+            function () {
+                const className = $(this).attr('class');
+                $('.list_left img').removeClass('active');
+                $('.list_left img.' + className).addClass('active');
+            },
+            function () {
+                $('.list_left img').removeClass('active'); // 마우스가 떠났을 때 다시 숨김 처리
+            }
+        );
+    });
 });
